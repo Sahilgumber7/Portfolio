d231b68e989241ebab3629e5c08b15ac4dcfdb01
sahilgumber7 <sahilgumber13418@gmail.com>
2025-05-27 15:05:02 +0530

    Initial commit

---
+++ src/components/DownLoadResumeBtn.tsx
@@ -0,0 +1,17 @@
+
+import { Download } from 'lucide-react'
+
+import React from 'react'
+
+import HackerBtn from './animation/HackerBtn'
+
+function DownLoadResumeBtn() {
+  return (
+    <div className="h-fit w-full mt-2 py-2 px-4">
+  
+    <HackerBtn label='Download Resume' />
+  </div>
+  )
+}
+
+export default DownLoadResumeBtn
